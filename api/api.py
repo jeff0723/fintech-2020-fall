42e914ebcd0bdf9cea8f996dfc91e68783f9c77f
Jeffrey Lin <qwertzxc1478963@gmail.com>
2021-06-16 09:56:40 +0800

    update

--- api/api.py
+++ api/api.py
@@ -1,20 +1,16 @@
-import time
+
 import flask
 import json
 import pandas as pd
 app = flask.Flask(__name__)
 model = pd.read_pickle('./model/xgboost_model.pickle')
-print(type(model))
+# print(type(model))
 category_columns = pd.read_pickle('./model/categorical_columns.pickle')
-print(type(category_columns))
+# print(type(category_columns))
 one_hot_columns = pd.read_pickle('./model/one_hot_columns.pickle')
-print(type(one_hot_columns))
+# print(type(one_hot_columns))
 
 
-print('successful load model')
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
 @app.route('/model', methods=['GET','POST'])
 def get_return():
     if flask.request.method == 'GET':
@@ -27,7 +23,7 @@ def get_return():
         # print(type(flask.request.data.decode('utf-8')))
         # s = flask.request.data.decode('utf-8')
         data = json.loads(flask.request.data.decode('utf-8'))
-        print(type(data))
+        # print(type(data))
 
         numeric = data['numerical_data']
         category = data['categorical_data']
@@ -36,9 +32,9 @@ def get_return():
         category_df = pd.DataFrame([[0]*len(category_columns)] ,columns=category_columns)
         category_df[columns] = 1
         input_data = pd.concat([numeric_df,category_df],axis=1)[one_hot_columns]
-        print(input_data.shape)
+        # print(input_data.shape)
         prob = model.predict_proba(input_data)[0][1]
         # print(type(data))
         # pd.to_pickle(data,'./test.pickle')
-        print("POST: I received your message")
+        # print("POST: I received your message")
         return {"probability":str(prob)}
